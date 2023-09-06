5029f0f3c5f2d6b03469e067bca3a0a06d9e3aa1
jx tsai <chihsun.tsai@gmail.com>
2023-09-06 22:37:11 +0800

    Delete python/LPHW directory

--- python/LPHW/test.py
+++
@@ -1,9 +0,0 @@
-def triple(a): 
-	"""return qrgument(number) -> exponenet trible number """
-	return a**3
-  
-a = 4  
-print "The number of %d" %a
-print "The triple of %d is: " %a ,
-print triple(a)
-
