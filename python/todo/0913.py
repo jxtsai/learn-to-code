82feb70fb836454a3597f8859643f944344716f2
jx tsai <chihsun.tsai@gmail.com>
2023-09-13 13:54:42 +0800

    Update print header

--- python/todo/0913.py
+++ python/todo/0913.py
@@ -1,5 +1,15 @@
 import sys
 
+header = """
+  _____         _           
+ |_   _|__   __| | ___  ___ 
+   | |/ _ \ / _` |/ _ \/ __|
+   | | (_) | (_| | (_) \__ \\
+   |_|\___/ \__,_|\___/|___/
+                            
+"""
+
+
 if sys.argv[1] == "add":
     with open("list.txt", "a+") as file: 
         file.write(sys.argv[2]+"\n")
@@ -15,5 +25,10 @@ elif sys.argv[1] == "remove":
         
 with open("list.txt", "r") as file: 
     content = file.readlines()
+    print(header)
+    print("Here is your ToDo tasks: "+"\n") 
     for idx, line in enumerate(content):
         print(idx+1, line.rstrip())        
+    
+    print()    
+    print("***********************************")    
