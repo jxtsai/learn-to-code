ce6c46318ac099e3c572fe1df904cf5aff452f16
jx tsai <chihsun.tsai@gmail.com>
2023-09-18 15:36:14 +0800

    Update bjack.py

--- python/bjack/bjack.py
+++ python/bjack/bjack.py
@@ -29,12 +29,46 @@ playerhand = []
 dealerhand = []
 
 def dealCard(turn):
-    pick = random.choice(deck)
-    turn.append(pick)
-    deck.remove(pick)
-
+    card = random.choice(deck)
+    print(card)
+    turn.append(card)
+    deck.remove(card)
+    
 def caculate(hand):
     '''
-    function parameter is a list of cardhand
+    function parameter is a list of cards
     '''
-    if 
+    faces = ["J","Q","K"]
+    total = 0
+    for c in hand:
+        if c in faces:
+            total = total + 10
+        else :
+            total = total + c
+        # elif c == "A" and total <= 17:
+        #    total = total + 11
+    if total > 21:
+       play = "q"
+       return "Busts!"
+    else:
+        return total
+ 
+def winner():
+    if caculate(playerhand) >= caculate(dealerhand):
+        return "You win!"
+    else:
+        return "You lost!"
+
+play = input("Add card, please type 'a', to quit this game, type 'q' !")
+
+while play != "q":
+    #print(playerhand)
+    #print(dealerhand)
+    dealCard(playerhand)
+    dealCard(dealerhand)
+    print(playerhand, dealerhand)
+    print(f"player hand are : {caculate(playerhand)}")
+    print(f"dealer's hand are : {caculate(dealerhand)}")
+    print(winner())
+    break
+    
